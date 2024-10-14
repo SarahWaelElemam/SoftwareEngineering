0733f27f351dab429211766c59e99c823af1ad89
hanaAbdelwahab <hana.m.abdelwahab3@gmail.com>
2024-10-14 23:44:52 +0300

    final Events page css and html

--- Events.php
+++ Events.php
@@ -1,9 +1,10 @@
-<?php include "NavBar.php" ?>
+<?php include "NavBar.php"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="Events.css">
     <title>Events</title>
     
@@ -31,72 +32,326 @@
         <div id="calendar"></div>
         <button id="closeModal">Close</button>
     </div>
-<!--<div class="ticket">
+</div>
+<!-- Row 1-->
+<div class="row-tick">
+    <!--Ticket 1-->
+<div class="ticket">
   <div class="side front">
-    <img src="https://www.marinabaysands.com/content/dam/singapore/marinabaysands/master/main/home/entertainment/captain-marvel/OPENWORLD_TT_final_18_1920x1234.jpg">
+    <img src="memopng.png">
     <div class="info bottom">
-      <h1>CAPTAIN MARVEL</h1>
-      <span class="title address">AMC Loweis Theater</span>
+      <h1>Memo</h1>
+      <span class="title address">Grand Nile Tower</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price Range: 500L.E - 2,500L.E</p>
       <dl>
-        <dt>Tickets</dt>
-        <dd>2</dd>
         <dt>Date</dt>
-        <dd>8 Mar</dd>
+        <dd>17 Oct</dd>
         <dt>Time</dt>
-        <dd>18:15</dd>
+        <dd>9:00pm</dd>
       </dl>
-      <span class="floating price">$24.50</span>
     </div>
   </div>
   <div class="side back">
     <div class="top">
       <div class="span">
-        <h2>Movie</h2>
-        <span>Captain Marvel</span>
+        <h2>Organized By</h2>
+        <span>Craft Media</span>
       </div>
       <div class="span">
-        <h2>Theater</h2>
-        <span>AMC Loweis, 86th Street</span>
-      </div>
-      <div class="span span8">
-        <h2>Date</h2>
-        <span>8 March,2019</span>
-      </div>
-      <div class="span span4">
-        <h2>Time</h2>
-        <span>18:15</span>
-      </div>
-      <div class="span span8">
-        <h2>Tickets</h2>
-        <span>2</span>
-      </div>
-      <div class="span span4">
-        <h2>Price</h2>
-        <span class="strong">$24.50</span>
+        <h2>Location</h2>
+        <span>Grand Nile Tower</span>
+        <p>Abdulaziz Al Saud, Old Cairo, Cairo</p>
       </div>
     </div>
     
     <div class="payment bottom">
-      <h1>PAYMENT DETAILS</h1>
-      <span class="floating"></span>
-      <div class="row card-num">
-        <input type="text" placeholder="0000" maxlength="4">
-        <input type="text" placeholder="0000" maxlength="4">
-        <input type="text" placeholder="0000" maxlength="4">
-        <input type="text" placeholder="0000" maxlength="4">
-      </div>
-      <div class="row">
-        <div class="span span4">
-          <input type="text" placeholder="MM/YY" maxlength="4">
-        </div>
-        <div class="span span4">
-          <input type="text" placeholder="CVV" maxlength="4"></div>
-        <div class="span span4">
-          <input type="text" placeholder="ZIP" maxlength="4"></div>
-      </div>
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
     </div>
   </div>
-</div>-->
+  
+</div>
+<!--Ticket 2-->
+<div class="ticket">
+  <div class="side front">
+    <img src="sahebna.jpg">
+    <div class="info bottom">
+      <h1>Sa7ebna</h1>
+      <span class="title address">AUC Tahrir, Falaki Theater</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 280L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>17 Oct</dd>
+        <dt>Time</dt>
+        <dd>8:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Act Theater Group</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>AUC Tahrir, Falaki Theater</span>
+        <p>Al Falaki, Abdeen, Cairo Governorate</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+<!--Ticket 3-->
+<div class="ticket">
+  <div class="side front">
+    <img src="The Comedy Bunch Live at Boom Roomjpg.jpg">
+    <div class="info bottom">
+      <h1>The Comedy Bunch Live</h1>
+      <span class="title address">Boom Room, Open Air Mall, Madinaty</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 350L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>15 Oct</dd>
+        <dt>Time</dt>
+        <dd>9:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Boom Room</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>Boom Room, Open Air Mall, Madinaty</span>
+        <p>Open Air Mall, parking Gate 3, Madinaty, Cairo</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+</div>
+<!-- Row 1-->
+<div class="row-tick">
+    <!--Ticket 1-->
+<div class="ticket">
+  <div class="side front">
+    <img src="memopng.png">
+    <div class="info bottom">
+      <h1>Memo</h1>
+      <span class="title address">Grand Nile Tower</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price Range: 500L.E - 2,500L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>17 Oct</dd>
+        <dt>Time</dt>
+        <dd>9:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Craft Media</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>Grand Nile Tower</span>
+        <p>Abdulaziz Al Saud, Old Cairo, Cairo</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+  
+</div>
+<!--Ticket 2-->
+<div class="ticket">
+  <div class="side front">
+    <img src="sahebna.jpg">
+    <div class="info bottom">
+      <h1>Sa7ebna</h1>
+      <span class="title address">AUC Tahrir, Falaki Theater</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 280L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>17 Oct</dd>
+        <dt>Time</dt>
+        <dd>8:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Act Theater Group</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>AUC Tahrir, Falaki Theater</span>
+        <p>Al Falaki, Abdeen, Cairo Governorate</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+<!--Ticket 3-->
+<div class="ticket">
+  <div class="side front">
+    <img src="The Comedy Bunch Live at Boom Roomjpg.jpg">
+    <div class="info bottom">
+      <h1>The Comedy Bunch Live</h1>
+      <span class="title address">Boom Room, Open Air Mall, Madinaty</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 350L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>15 Oct</dd>
+        <dt>Time</dt>
+        <dd>9:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Boom Room</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>Boom Room, Open Air Mall, Madinaty</span>
+        <p>Open Air Mall, parking Gate 3, Madinaty, Cairo</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+</div>
+<!-- Row 1-->
+<div class="row-tick">
+    <!--Ticket 1-->
+<div class="ticket">
+  <div class="side front">
+    <img src="memopng.png">
+    <div class="info bottom">
+      <h1>Memo</h1>
+      <span class="title address">Grand Nile Tower</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price Range: 500L.E - 2,500L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>17 Oct</dd>
+        <dt>Time</dt>
+        <dd>9:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Craft Media</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>Grand Nile Tower</span>
+        <p>Abdulaziz Al Saud, Old Cairo, Cairo</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+  
+</div>
+<!--Ticket 2-->
+<div class="ticket">
+  <div class="side front">
+    <img src="sahebna.jpg">
+    <div class="info bottom">
+      <h1>Sa7ebna</h1>
+      <span class="title address">AUC Tahrir, Falaki Theater</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 280L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>17 Oct</dd>
+        <dt>Time</dt>
+        <dd>8:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Act Theater Group</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>AUC Tahrir, Falaki Theater</span>
+        <p>Al Falaki, Abdeen, Cairo Governorate</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+<!--Ticket 3-->
+<div class="ticket">
+  <div class="side front">
+    <img src="The Comedy Bunch Live at Boom Roomjpg.jpg">
+    <div class="info bottom">
+      <h1>The Comedy Bunch Live</h1>
+      <span class="title address">Boom Room, Open Air Mall, Madinaty</span>
+      <p><i class="fa-solid fa-circle" style="color: #03b300;"></i> Price: 350L.E</p>
+      <dl>
+        <dt>Date</dt>
+        <dd>15 Oct</dd>
+        <dt>Time</dt>
+        <dd>9:00pm</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="side back">
+    <div class="top">
+      <div class="span">
+        <h2>Organized By</h2>
+        <span>Boom Room</span>
+      </div>
+      <div class="span">
+        <h2>Location</h2>
+        <span>Boom Room, Open Air Mall, Madinaty</span>
+        <p>Open Air Mall, parking Gate 3, Madinaty, Cairo</p>
+      </div>
+    </div>
+    
+    <div class="payment bottom">
+     <button class="Book"><i class="fa-solid fa-ticket"></i> Book Now</button>
+    </div>
+  </div>
+</div>
+</div>  
 <script src="Events.js"></script>
 </body>
 </html>
+<?php include "Footer.php"?>
