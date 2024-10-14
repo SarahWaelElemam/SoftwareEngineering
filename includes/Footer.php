35f440fb09cd4ef64759dd9ae88d9203d4f5b3f5
ismailtaha12 <144679981+ismailtaha12@users.noreply.github.com>
2024-10-15 02:59:16 +0300

    added ticket count pop up and setup folder structure

--- includes/Footer.php
+++ includes/Footer.php
@@ -4,16 +4,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>TixCarte</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../assets/css/Footer.css" />
     <script
       src="https://kit.fontawesome.com/19a37f6564.js"
       crossorigin="anonymous"
     ></script>
   </head>
   <body>
-    <h1>content</h1>
-    <h1>content</h1>
-    <h1>content</h1>
+    
 
     <footer>
       <div class="footer-container">
--- includes/NavBar.php
+++ includes/NavBar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TixCarte - Responsive Navigation</title>
-    <link rel="stylesheet" href="NavBar.css">
+    <link rel="stylesheet" href="../assets/css/NavBar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
@@ -33,6 +33,6 @@
             </div>
         </nav>
     </header>
-    <script src="NavBar.js"></script>
+    <script src="../assets/js/NavBar.js"></script>
 </body>
 </html>
