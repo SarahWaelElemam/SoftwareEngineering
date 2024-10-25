630267aea3665ad503ff0281dacdee5db1986dea
hanaAbdelwahab <hana.m.abdelwahab3@gmail.com>
2024-10-25 20:05:47 +0300

    user dashboard

---
+++ User.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="User.css" />
+    <title>Movie Seat Booking</title>
+</head>
+<body>
+    <?php include"NavBar.php"?>
+    <section>
+  <div class="profile py-4">
+    <div class="container">
+      <div class="row">
+        <div class="card shadow-sm">
+          <div class="card-header bg-transparent text-center">
+            <div class="profile-content">
+              <!-- Avatar on the Left -->
+              <div id="avatar-display" class="avatar-container">
+                <!-- Avatar image or content here -->
+              </div>
+            </div>
+            <div class="profile-details">
+                <h3>Hana Mohamed</h3>
+                <p>Member since 2024</p>
+              <div class="table table-bordered">
+                <p><i class="fa-regular fa-calendar" style="color: #FFD43B;"></i> 10 May 2023</p>
+                <p><i class="fa-solid fa-phone" style="color: #FFD43B;"></i> +20 123 456 7890</p>
+                <p><i class="fa-regular fa-user" style="color: #FFD43B;"></i> Female</p>
+                <p><i class="fa-solid fa-location-dot" style="color: #FFD43B;"></i> Cairo, Egypt</p>
+                <p><i class="fa-regular fa-envelope" style="color: #FFD43B;"></i> hana2200912@miuegypt.edu.eg</p>
+              </div>
+            </div>
+          <div style="height: 1em"></div>
+        </div>
+        <div class="buttons">
+            <button type="button" class="btn" id="btn1">Edit Profile</button>
+            <button type="button" class="btn" id="btn3">Delete Account</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    
+    
+</body>
+</html>
