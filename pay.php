575d32f7fa7e0d9c11ef29f3023ff49c9069db48
karengeorgee <168434606+karengeorgee@users.noreply.github.com>
2024-10-25 12:42:48 +0300

    About us done

--- pay.php
+++ pay.php
@@ -56,9 +56,34 @@
     </div>
     </div>
     <div class="info">
-        <p>hena el info</p>
+    <img src="memopng.png" alt="Memo">
+    <div class="details">
+        <p class="name">MEMO-7 NOV 2024</p>
+        <p class="date">Nov 07 | 09:00pm</p>
+    </div>
+    <div class="containerr">
+  <div class="steps">
+    <div class="circle-wrapper">
+      <div class="circle active">1</div><span>Select Ticket</span>
+    </div>
+    <div class="progress-bar"><span class="indicator"></span></div>
+    <div class="circle-wrapper">
+      <div class="circle">2</div><span>Review and Checkout</span>
+    </div>
+    <div class="progress-bar"><span class="indicator"></span></div>
+    <div class="circle-wrapper">
+      <div class="circle">3</div><span>Send Ticket</span>
+    </div>
+  </div>
+  <div class="buttons">
+    <button id="prev" disabled>Previous</button>
+    <button id="next">Next</button>
+  </div>
+</div>
+
     </div>
     </div>
     <script src="pay.js"></script>
+    <script src="progress.js"></script>
 </body>
 </html>
